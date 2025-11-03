861b65e2801740e57b08fb14bcab339ffa266238
Artyom Boyarov <artyom.boyarov@gmail.com>
2025-11-03 13:29:15 -0500

    Added code skeleton for UI

---
+++ data_collection/data_collection_ui.py
@@ -0,0 +1,4 @@
+### Write a Python script to display the following acceleration data over time
+
+times = [0, 1, 2, 3, 4, 5]
+accelerations = [[0, 0, 9.8], [0.5, 0.2, 9.7], [1.0, 0.4, 9.6], [1.5, 0.6, 9.5], [2.0, 0.8, 9.4], [2.5, 1.0, 9.3]]
