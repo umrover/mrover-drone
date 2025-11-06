dc4bae0b8490064fac8d15f7016598817044a382
Artyom Boyarov <artyom.boyarov@gmail.com>
2025-11-06 15:00:42 -0500

    Got drone_sim to build and launch

--- drone_sim/setup.py
+++ drone_sim/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'drone_sim'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     py_modules=[],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
